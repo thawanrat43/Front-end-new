8d2e4055819f38f9cb841fd647e2b31d98bcfed2
thawanrat43 <129850059+thawanrat43@users.noreply.github.com>
2023-09-03 17:24:16 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,18 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    proxy:{
+      '/api':{
+        target: 'https://back-end-update.onrender.com',
+           changeOrigin: true,
+           secure: false,      
+           ws: true,
+      }
+    }
+  },
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
   plugins: [react()],
 })
